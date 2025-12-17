35f7d7ea4a350d9d16dc362f1bd8ee390475783e
Svetlana Kapelko <56638107+Svkapelko@users.noreply.github.com>
2025-12-17 12:14:04 +0300

    Доделала перебор карточек ресторанов и перебор карточек продуктов, добавила стили на страницы Home и Product

--- Vues/Home.vue
+++ Vues/Home.vue
@@ -1,184 +1,226 @@
 <script setup>
+const restArray = [
+  {
+    id: 0,
+    title: "Пицца плюс",
+    time: 50,
+    rating: 4.5,
+    price: 900,
+    type: "Пицца",
+    img: "rest1.jpg",
+  },
+  {
+    id: 1,
+    title: "Тануки",
+    time: 45,
+    rating: 4.6,
+    price: 850,
+    type: "Суши",
+    img: "rest2.jpg",
+  },
+  {
+    id: 2,
+    title: "FoodBand",
+    time: 30,
+    rating: 4.8,
+    price: 1000,
+    type: "Европейская еда",
+    img: "rest3.jpg",
+  },
+  {
+    id: 3,
+    title: "Жадина-пицца",
+    time: 45,
+    rating: 4.7,
+    price: 750,
+    type: "Пицца",
+    img: "rest4.jpg",
+  },
+  {
+    id: 4,
+    title: "Точка еды",
+    time: 35,
+    rating: 4.7,
+    price: 500,
+    type: "Европейская еда",
+    img: "rest5.jpg",
+  },
+  {
+    id: 5,
+    title: "PizzaBurger",
+    time: 50,
+    rating: 4.9,
+    price: 600,
+    type: "Пицца, бургер",
+    img: "rest6.jpg",
+  },
+];
 
-
+const getUrl = (name) => {
+  return new URL(`../src/assets/images/rests/${name}`, import.meta.url);
+};
+/* Метод getUrl(name) предназначен для формирования корректного абсолютного пути к изображению/др ресурсу в проекте. Метод возвращает объект типа URL, содержащий абсолютный путь к файлу. Эта конструкция полезна, потому что new URL() формирует правильный URL, учитывающий расположение текущего модуля (import.meta.url).
+    Механизм работы:
+- Сначала создается относительная ссылка на файл (../src/assets/images/goods/${name}).
+ - Далее эта относительная ссылка преобразуется в абсолютную путем привязки к базовому URL (import.meta.url), который представляет собой URL исходного JavaScript-модуля.
+    Зачем это нужно?
+В большинстве случаев, при разработке фронтенда с использованием инструментов сборки (Webpack/Vite), статические ресурсы, такие как изображения, собираются и обрабатываются сборщиком. Эти инструменты могут перемещать файлы или менять их имена, создавая хэшированные названия для кэширования. Поэтому прямой доступ к путям может стать проблемой. Используя метод getUrl, вы гарантируете, что любые преобразования имен файлов и местоположений учитываются сборочной системой, и ссылки остаются корректными независимо от окружения (разработка, продакшн).
+*/
 </script>
 
 <template>
-    <main class="main">
-        <section class="banner">
-            <div class="container">
-                <div class="banner-item">
-                    <h2 class="banner-item--title">Онлайн-сервис <br> доставки еды на дом</h2>
-                    <p class="banner-item--subtitle">Блюда из любимого ресторана привезет курьер в перчатках, маске и с
-                        антисептиком</p>
-                </div>
+  <main class="main">
+    <section class="banner">
+      <div class="container">
+        <div class="banner-item">
+          <h2 class="banner-item--title">
+            Онлайн-сервис <br />
+            доставки еды на дом
+          </h2>
+          <p class="banner-item--subtitle">
+            Блюда из любимого ресторана привезет курьер в перчатках, маске и с
+            антисептиком
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="products">
+      <div class="container">
+        <div class="products-header">
+          <h3 class="products-header--title">Рестораны</h3>
+          <input
+            type="text"
+            placeholder="Поиск блюд и ресторанов"
+            class="products-header--search"
+          />
+        </div>
+        <div class="products-wrapper" id="rests-container">
+          <router-link
+            v-for="rest in restArray"
+            :key="rest.id"
+            :to="`/rest?id=${rest.id}`"
+            class="products-card"
+          >
+            <div class="products-card_image">
+              <img :src="getUrl(rest.img)" alt="изображение ресторана" />
             </div>
-        </section>
-        <section class="products">
-            <div class="container">
-                <div class="products-header">
-                    <h3 class="products-header--title">Рестораны</h3>
-                    <input type="text" placeholder="Поиск блюд и ресторанов" class="products-header--search">
+            <div class="products-card_description">
+              <div class="products-card_description--row">
+                <h4 class="products-card_description--title">
+                  {{ rest.title }}
+                </h4>
+                <div class="products-card_description--badge">
+                  {{ rest.time }} мин
                 </div>
-                <div class="products-wrapper" id="rests-container">
-                    <router-link to="/rest" class="products-card" >
-                        <div class="products-card_image">
-                            <img src="../src/assets/images/rests/rest1.jpg" alt="rest1">
-                        </div>
-                        <div class="products-card_description">
-                            <div class="products-card_description--row">
-                                <h4 class="products-card_description--title">Пицца плюс</h4>
-                                <div class="products-card_description--badge">50мин</div>
-                            </div>
-                            <div class="products-card_description--row">
-                                <div class="products-card_description--info">
-                                    <div class="products-card_description--rating">
-                                        <img src="../src/assets/images/icons/star.svg" alt="star">
-                                        4.5
-                                    </div>
-                                    <div class="products-card_description--price">
-                                        От 900 ₽
-                                    </div>
-                                    <div class="products-card_description--group">
-                                        Пицца
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </router-link>
-                    <router-link to="/rest" class="products-card">
-                        <div class="products-card_image">
-                            <img src="../src/assets/images/rests/rest2.jpg" alt="rest2">
-                        </div>
-                        <div class="products-card_description">
-                            <div class="products-card_description--row">
-                                <h4 class="products-card_description--title">Тануки</h4>
-                                <div class="products-card_description--badge">45мин</div>
-                            </div>
-                            <div class="products-card_description--row">
-                                <div class="products-card_description--info">
-                                    <div class="products-card_description--rating">
-                                        <img src="../src/assets/images/icons/star.svg" alt="star">
-                                        4.6
-                                    </div>
-                                    <div class="products-card_description--price">
-                                        От 850 ₽
-                                    </div>
-                                    <div class="products-card_description--group">
-                                        Суши
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </router-link>
-                    <router-link to="/rest" class="products-card">
-                        <div class="products-card_image">
-                            <img src="../src/assets/images/rests/rest3.jpg" alt="rest3">
-                        </div>
-                        <div class="products-card_description">
-                            <div class="products-card_description--row">
-                                <h4 class="products-card_description--title">FoodBand</h4>
-                                <div class="products-card_description--badge">30мин</div>
-                            </div>
-                            <div class="products-card_description--row">
-                                <div class="products-card_description--info">
-                                    <div class="products-card_description--rating">
-                                        <img src="../src/assets/images/icons/star.svg" alt="star">
-                                        4.8
-                                    </div>
-                                    <div class="products-card_description--price">
-                                        От 1000 ₽
-                                    </div>
-                                    <div class="products-card_description--group">
-                                        Европейская еда
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </router-link>
-                    <router-link to="/rest" class="products-card">
-                        <div class="products-card_image">
-                            <img src="../src/assets/images/rests/rest4.jpg" alt="rest4">
-                        </div>
-                        <div class="products-card_description">
-                            <div class="products-card_description--row">
-                                <h4 class="products-card_description--title">Жадина-пицца</h4>
-                                <div class="products-card_description--badge">45мин</div>
-                            </div>
-                            <div class="products-card_description--row">
-                                <div class="products-card_description--info">
-                                    <div class="products-card_description--rating">
-                                        <img src="../src/assets/images/icons/star.svg" alt="star">
-                                        4.7
-                                    </div>
-                                    <div class="products-card_description--price">
-                                        От 750 ₽
-                                    </div>
-                                    <div class="products-card_description--group">
-                                        Пицца
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </router-link>
-                    <router-link to="/rest" class="products-card">
-                        <div class="products-card_image">
-                            <img src="../src/assets/images/rests/rest5.jpg" alt="rest5">
-                        </div>
-                        <div class="products-card_description">
-                            <div class="products-card_description--row">
-                                <h4 class="products-card_description--title">Точка еды</h4>
-                                <div class="products-card_description--badge">35мин</div>
-                            </div>
-                            <div class="products-card_description--row">
-                                <div class="products-card_description--info">
-                                    <div class="products-card_description--rating">
-                                        <img src="../src/assets/images/icons/star.svg" alt="star">
-                                        4.7
-                                    </div>
-                                    <div class="products-card_description--price">
-                                        От 500 ₽
-                                    </div>
-                                    <div class="products-card_description--group">
-                                        Европейская еда
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </router-link>
-                    <router-link to="/rest" class="products-card">
-                        <div class="products-card_image">
-                            <img src="../src/assets/images/rests/rest6.jpg" alt="rest6">
-                        </div>
-                        <div class="products-card_description">
-                            <div class="products-card_description--row">
-                                <h4 class="products-card_description--title">PizzaBurger</h4>
-                                <div class="products-card_description--badge">50мин</div>
-                            </div>
-                            <div class="products-card_description--row">
-                                <div class="products-card_description--info">
-                                    <div class="products-card_description--rating">
-                                        <img src="../src/assets/images/icons/star.svg" alt="star">
-                                        4.9
-                                    </div>
-                                    <div class="products-card_description--price">
-                                        От 600 ₽
-                                    </div>
-                                    <div class="products-card_description--group">
-                                        Пицца, бургер
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </router-link>
+              </div>
+              <div class="products-card_description--row">
+                <div class="products-card_description--info">
+                  <div class="products-card_description--rating">
+                    <img src="../src/assets/images/icons/star.svg" alt="star" />
+                    {{ rest.rating }}
+                  </div>
+                  <div class="products-card_description--price">
+                    От {{ rest.price }} ₽
+                  </div>
+                  <div class="products-card_description--group">
+                    {{ rest.type }}
+                  </div>
                 </div>
+              </div>
             </div>
-        </section>
-    </main>
+          </router-link>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.products-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap; /* чтобы был перенос текста при уменьшении ширины экрана*/
+  gap: 24px;
+}
+.products-header--title {
+  color: rgba(0, 0, 0, 1);
+  font-size: 36px;
+  font-weight: 700;
+  margin: 0;
+}
+.products-header--search {
+  box-sizing: border-box;
+  border: 1px solid rgba(217, 217, 217, 1);
+  border-radius: 2px;
+  background: rgba(255, 255, 255, 1);
+  min-width: 306px;
+  height: 34px;
+  flex-shrink: 0;
+  padding: 5px 12px;
+}
+.products-header--search:focus {
+  outline: none;
+  border: 1px solid rgba(217, 217, 217, 1);
+}
+.products-header--search::placeholder {
+  color: rgba(191, 191, 191, 1);
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+}
+.products-card_description--title {
+  color: rgba(0, 0, 0, 1);
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 32px;
+  margin: 0; /* убираем лишние отступы */
+}
+.products-card_description--badge {
+  color: rgba(255, 255, 255, 1);
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 20px;
+  border-radius: 2px;
+  background: rgba(38, 38, 38, 1);
+  padding: 1px 8px;
+}
+.products-card_description--info {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap; /* перенос строки */
+  row-gap: 8px;
+  column-gap: 25px;
+}
+.products-card_description--rating {
+  color: rgba(255, 193, 7, 1);
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 32px;
+}
+.products-card_description--price {
+  color: rgba(140, 140, 140, 1);
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 32px;
+}
+.products-card_description--group {
+  color: rgba(140, 140, 140, 1);
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 32px;
+  position: relative;
+}
+.products-card_description--group::before {
+  content: ""; /* точку задаем через псевдокласс*/
+  display: block;
+  width: 5px;
+  height: 5px;
+  border-radius: 50%;
+  background-color: rgba(140, 140, 140, 1);
+  position: absolute;
+  top: 14px;
+  left: -14px;
+}
+</style>
--- Vues/Product.vue
+++ Vues/Product.vue
@@ -1,4 +1,52 @@
 <script setup>
+const goodsArray = [
+  {
+    id: 0,
+    title: "Ролл угорь стандарт",
+    info: "Рис, угорь, соус унаги, кунжут, водоросли нори.",
+    price: 250,
+    img: "prd1.jpg",
+  },
+  {
+    id: 1,
+    title: "Калифорния лосось стандарт",
+    info: "Рис, лосось, авокадо, огурец, майонез, икра масаго, водоросли нори.",
+    price: 395,
+    img: "prd2.jpg",
+  },
+  {
+    id: 2,
+    title: "Окинава стандарт",
+    info: "Рис, креветка отварная, сыр сливочный, лосось, огурец свежий.",
+    price: 340,
+    img: "prd3.jpg",
+  },
+  {
+    id: 3,
+    title: "Цезарь маки хl",
+    info: "Рис, куриная грудка копченая, икра масаго, томат, айсберг, соус цезарь...",
+    price: 465,
+    img: "prd4.jpg",
+  },
+  {
+    id: 4,
+    title: "Ясай маки стандарт 185 г",
+    info: "Рис, помидор свежий, перец болгарский, авокадо, огурец, айсберг.",
+    price: 290,
+    img: "prd5.jpg",
+  },
+  {
+    id: 5,
+    title: "Ролл с креветкой стандарт",
+    info: "Рис, водоросли нори, креветки отварные, сыр сливочный, огурцы.",
+    price: 380,
+    img: "prd6.jpg",
+  },
+];
+
+const getUrl = (name) => {
+  return new URL(`../src/assets/images/goods/${name}`, import.meta.url);
+};
 </script>
 
 <template>
@@ -6,171 +54,32 @@
     <section class="products">
       <div class="container">
         <div class="products-wrapper" id="goods-container">
-          <div class="products-card">
+          <div v-for="item in goodsArray" :key="item.id" class="products-card">
             <div class="products-card_image">
-              <img src="../src/assets/images/goods/prd1.jpg" alt="product1" />
+              <img :src="getUrl(item.img)" alt="изображение продукта" />
             </div>
             <div class="products-card_description">
               <div class="products-card_description--row">
                 <h5 class="products-card_description--name">
-                  Ролл угорь стандарт
+                  {{ item.title }}
                 </h5>
               </div>
               <div class="products-card_description--row">
                 <p class="products-card_description--text">
-                  Рис, угорь, соус унаги, кунжут, водоросли нори.
+                  {{ item.info }}
                 </p>
               </div>
               <div class="products-card_description--row">
                 <div class="products-card_description-controls">
                   <button class="btn btn-primary">
                     В корзину
-                    <img src="../src/assets/images/icons/cart.svg" alt="shopping-cart" />
+                    <img
+                      src="../src/assets/images/icons/cart.svg"
+                      alt="shopping-cart"
+                    />
                   </button>
-                  <span class="products-card_description-controls--price"
-                    >250 ₽
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="products-card">
-            <div class="products-card_image">
-              <img src="../src/assets/images/goods/prd2.jpg" alt="product2" />
-            </div>
-            <div class="products-card_description">
-              <div class="products-card_description--row">
-                <h5 class="products-card_description--name">
-                  Калифорния лосось стандарт
-                </h5>
-              </div>
-              <div class="products-card_description--row">
-                <p class="products-card_description--text">
-                  Рис, лосось, авокадо, огурец, майонез, икра масаго, водоросли
-                  нори.
-                </p>
-              </div>
-              <div class="products-card_description--row">
-                <div class="products-card_description-controls">
-                  <button class="btn btn-primary">
-                    В корзину
-                    <img src="../src/assets/images/icons/cart.svg" alt="shopping-cart" />
-                  </button>
-                  <span class="products-card_description-controls--price"
-                    >395 ₽
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="products-card">
-            <div class="products-card_image">
-              <img src="../src/assets/images/goods/prd3.jpg" alt="product3" />
-            </div>
-            <div class="products-card_description">
-              <div class="products-card_description--row">
-                <h5 class="products-card_description--name">
-                  Окинава стандарт
-                </h5>
-              </div>
-              <div class="products-card_description--row">
-                <p class="products-card_description--text">
-                  Рис, креветка отварная, сыр сливочный, лосось, огурец свежий.
-                </p>
-              </div>
-              <div class="products-card_description--row">
-                <div class="products-card_description-controls">
-                  <button class="btn btn-primary">
-                    В корзину
-                    <img src="../src/assets/images/icons/cart.svg" alt="shopping-cart" />
-                  </button>
-                  <span class="products-card_description-controls--price"
-                    >340 ₽
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="products-card">
-            <div class="products-card_image">
-              <img src="../src/assets/images/goods/prd4.jpg" alt="product4" />
-            </div>
-            <div class="products-card_description">
-              <div class="products-card_description--row">
-                <h5 class="products-card_description--name">Цезарь маки хl</h5>
-              </div>
-              <div class="products-card_description--row">
-                <p class="products-card_description--text">
-                  Рис, куриная грудка копченая, икра масаго, томат, айсберг,
-                  соус цезарь...
-                </p>
-              </div>
-              <div class="products-card_description--row">
-                <div class="products-card_description-controls">
-                  <button class="btn btn-primary">
-                    В корзину
-                    <img src="../src/assets/images/icons/cart.svg" alt="shopping-cart" />
-                  </button>
-                  <span class="products-card_description-controls--price"
-                    >465 ₽
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="products-card">
-            <div class="products-card_image">
-              <img src="../src/assets/images/goods/prd5.jpg" alt="product5" />
-            </div>
-            <div class="products-card_description">
-              <div class="products-card_description--row">
-                <h5 class="products-card_description--name">
-                  Ясай маки стандарт 185 г
-                </h5>
-              </div>
-              <div class="products-card_description--row">
-                <p class="products-card_description--text">
-                  Рис, помидор свежий, перец болгарский, авокадо, огурец,
-                  айсберг.
-                </p>
-              </div>
-              <div class="products-card_description--row">
-                <div class="products-card_description-controls">
-                  <button class="btn btn-primary">
-                    В корзину
-                    <img src="../src/assets/images/icons/cart.svg" alt="shopping-cart" />
-                  </button>
-                  <span class="products-card_description-controls--price"
-                    >290 ₽
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="products-card">
-            <div class="products-card_image">
-              <img src="../src/assets/images/goods/prd6.jpg" alt="product6" />
-            </div>
-            <div class="products-card_description">
-              <div class="products-card_description--row">
-                <h5 class="products-card_description--name">
-                  Ролл с креветкой стандарт
-                </h5>
-              </div>
-              <div class="products-card_description--row">
-                <p class="products-card_description--text">
-                  Рис, водоросли нори, креветки отварные, сыр сливочный, огурцы.
-                </p>
-              </div>
-              <div class="products-card_description--row">
-                <div class="products-card_description-controls">
-                  <button class="btn btn-primary">
-                    В корзину
-                    <img src="../src/assets/images/icons/cart.svg" alt="shopping-cart" />
-                  </button>
-                  <span class="products-card_description-controls--price"
-                    >380 ₽
+                  <span class="products-card_description-controls--price">
+                    {{ item.price }} ₽
                   </span>
                 </div>
               </div>
@@ -182,4 +91,33 @@
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.products-card_description--name {
+  color: rgba(0, 0, 0, 1);
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 32px;
+  margin: 0;
+}
+.products-card_description--text {
+  color: rgba(140, 140, 140, 1);
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 21px;
+  margin: 0;
+}
+.products-card_description-controls {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+  row-gap: 10px;
+  column-gap: 30px;
+}
+.products-card_description-controls--price {
+  color: rgba(0, 0, 0, 1);
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 32px;
+}
+</style>
